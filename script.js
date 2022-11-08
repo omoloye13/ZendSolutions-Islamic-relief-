e465446f94fa8e91a994baf3a155651b1ccac528
omoloye13 <omoloyefauziyat@gmail.com>
2022-11-08 13:03:24 +0000

    code addition and refactor

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    // location.replace("donation.html")
+    window.location.href = "donation.html";
+  }
+  function myHome() {
+    // location.replace("donation.html")
+    window.location.href = "index.html";
+  }
+  function myReverse() {
+    // location.replace("donation.html")
+    window.location.href = "index.html";
+  }
+  function myForward() {
+    // location.replace("donation.html")
+    window.location.href = "details.html";
+  }
