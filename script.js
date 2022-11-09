a9daf4932fe6654484e0cac2f5441f956b164bf2
omoloye13 <omoloyefauziyat@gmail.com>
2022-11-09 14:16:33 +0000

    completed the details page

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function myFunction() {
     // location.replace("donation.html")
     window.location.href = "details.html";
   }
+  function myAddress() {
+    let myDiv = document.querySelector(".hide");
+    let myHeight = document.querySelector(".box")
+    myHeight.style.display.height ="220vh";
+    if (myDiv.style.display === "none") {
+      myDiv.style.display = "block";
+    } else {
+      myDiv.style.display = "none";
+    }
+  }
+  function myReverseTwo() {
+    // location.replace("donation.html")
+    window.location.href = "donation.html";
+  }
+  function myForwardTwo() {
+    // location.replace("donation.html")
+    window.location.href = "payment.html";
+  }
